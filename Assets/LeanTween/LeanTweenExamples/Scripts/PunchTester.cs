6e21159528eec2455c4b5344e997629b1d1fb5b7
Russell Savage <dentedpixel@gmail.com>
2013-12-17 12:39:32 -0500

    Fixed punch

---
+++ Assets/LeanTween/LeanTweenExamples/Scripts/PunchTester.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class PunchTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () 
+    {        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            LeanTween.scale(this.gameObject, Vector3.one, 1.0f).setEase(LeanTweenType.punch);            
+            print("scale punch!");
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            LeanTween.rotate(this.gameObject, Vector3.one, 1.0f).setEase(LeanTweenType.punch);
+            print("rotate punch!");
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            LeanTween.move(this.gameObject, new Vector3(0f,0f,1f), 1.0f).setEase(LeanTweenType.punch);
+            print("move punch!");
+        }
+	}
+}
