ec6bfbcc7cbac11bdb3c721b068c0eb686735d7e
Russell Savage <dentedpixel@gmail.com>
2014-03-15 16:18:00 -0400

    Added functioning CatMull Spline example

--- Assets/LeanTween/LeanTweenExamples/Scripts/ExampleCatmull.cs
+++ Assets/LeanTween/LeanTweenExamples/Scripts/ExampleCatmull.cs
@@ -9,14 +9,12 @@ public class ExampleCatmull : MonoBehaviour {
 	private GameObject ltLogo;
 
 	void Start () {
-		Vector3 diff = trans[1].position - trans[0].position;
-		cr = new LTSpline( new Vector3[] {trans[0].position-diff, trans[0].position, trans[1].position, trans[2].position, trans[2].position+diff} );
+		cr = new LTSpline( new Vector3[] {trans[0].position, trans[1].position, trans[2].position, trans[3].position, trans[4].position} );
 		ltLogo = GameObject.Find("LeanTweenLogo");
 	}
 	
 	private float iter;
 	void Update () {
-		Debug.Log("iter:"+iter);
 		ltLogo.transform.position = cr.point( iter /*(Time.time*1000)%1000 * 1.0 / 1000.0 */);
 
 		iter += Time.deltaTime*0.1f;
@@ -26,6 +24,6 @@ public class ExampleCatmull : MonoBehaviour {
 
 	void OnDrawGizmos(){
 		if(cr!=null)
-			cr.gizmoDraw( iter );
+			cr.gizmoDraw();
 	}
 }
