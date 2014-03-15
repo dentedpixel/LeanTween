14bc35154dc6924815df16fce92d5bb233635a48
Russell Savage <dentedpixel@gmail.com>
2014-03-15 16:24:43 -0400

    Spline example

--- Assets/LeanTween/LeanTweenExamples/Scripts/ExampleSpline.cs
+++ Assets/LeanTween/LeanTweenExamples/Scripts/ExampleSpline.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using System.Collections;
 
-public class ExampleCatmull : MonoBehaviour {
+public class ExampleSpline : MonoBehaviour {
 
 	public Transform[] trans;
 
 	LTSpline cr;
 	private GameObject ltLogo;
+	private GameObject ltLogo2;
 
 	void Start () {
 		cr = new LTSpline( new Vector3[] {trans[0].position, trans[1].position, trans[2].position, trans[3].position, trans[4].position} );
-		ltLogo = GameObject.Find("LeanTweenLogo");
+		ltLogo = GameObject.Find("LeanTweenLogo1");
+		ltLogo2 = GameObject.Find("LeanTweenLogo2");
+
+		LeanTween.moveSpline( ltLogo2, new Vector3[] {trans[0].position, trans[1].position, trans[2].position, trans[3].position, trans[4].position}, 1f).setEase(LeanTweenType.easeInOutQuad).setLoopPingPong().setOrientToPath(true);
 	}
 	
 	private float iter;
