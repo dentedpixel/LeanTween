e26c08cf29b38bac244c1c12bf5e7c25f4098244
Russell Savage <dentedpixel@gmail.com>
2015-01-24 08:24:25 -0500

    Documentation is now provided as a set of HTML files.

---
+++ Assets/LeanTween/Editor/LeanTweenDocumentationEditor.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class LeanTweenDocumentationEditor : Editor {
+
+	[MenuItem ("Lean/LeanTween Documentation")]
+	static void openDocumentation()
+	{
+		string documentationPath = "file://"+Application.dataPath + "/LeanTween/LeanTweenDocumentation/index.html";
+		Application.OpenURL(documentationPath);
+	}
+
+	[MenuItem ("Lean/LeanTween Forum (ask questions)")]
+	static void openForum()
+	{
+		Application.OpenURL("http://forum.unity3d.com/threads/leantween-a-tweening-engine-that-is-up-to-5x-faster-than-competing-engines.161113/");
+	}
+
+	[MenuItem ("Lean/LeanTween GitHub (contribute code)")]
+	static void openGit()
+	{
+		Application.OpenURL("https://github.com/dentedpixel/LeanTween");
+	}
+}
