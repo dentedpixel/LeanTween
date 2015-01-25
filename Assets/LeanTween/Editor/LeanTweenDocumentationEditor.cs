71e26a17f7a2d417dffb23bc181bcbde6a5ef3b9
Russell Savage <dentedpixel@gmail.com>
2015-01-25 18:10:38 -0500

    Made sure the documentation links still work if you move the LeanTween folder.

--- Assets/LeanTween/Editor/LeanTweenDocumentationEditor.cs
+++ Assets/LeanTween/Editor/LeanTweenDocumentationEditor.cs
@@ -7,8 +7,27 @@ public class LeanTweenDocumentationEditor : Editor {
 	[MenuItem ("Lean/LeanTween Documentation")]
 	static void openDocumentation()
 	{
+		#if !UNITY_3_5 && !UNITY_4_0 && !UNITY_4_0_1 && !UNITY_4_1 && !UNITY_4_2 && !UNITY_4_3
+		// Loops through all items in case the user has moved the default installation directory
+		string[] guids = AssetDatabase.FindAssets ("LeanTween", null);
+		string documentationPath = "";
+		foreach (string guid in guids){
+			string path = AssetDatabase.GUIDToAssetPath(guid);
+			if(path.IndexOf("classes/LeanTween.html")>=0){
+				documentationPath = path;
+				break;
+			}
+		}
+		documentationPath = documentationPath.Substring(documentationPath.IndexOf("/"));
+		string browserPath = "file://" + Application.dataPath + documentationPath + "#index";
+		Application.OpenURL(browserPath);
+
+		#else
+		// assumes the default installation directory
 		string documentationPath = "file://"+Application.dataPath + "/LeanTween/Documentation/classes/LeanTween.html#index";
 		Application.OpenURL(documentationPath);
+
+		#endif
 	}
 
 	[MenuItem ("Lean/LeanTween Forum (ask questions)")]
