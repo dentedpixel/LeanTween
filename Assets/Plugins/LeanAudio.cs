3dda57bf60910b6f04ca8850827cdfa78971d9a5
Russell Savage <dentedpixel@gmail.com>
2016-04-05 18:49:14 -0400

    Updated with latest LeanAudio (version submitted to Asset Store) **for reals this time**

--- Assets/Plugins/LeanAudio.cs
+++ Assets/Plugins/LeanAudio.cs
@@ -341,4 +341,78 @@ public class LeanAudioOptions : object {
 	public LeanAudioOptions(){}
 
 	/**
-	* Set the frequency for the audio is encoded. 44100 is CD quality, but you can usually get away with much lower (or use a lower amount to 
+	* Set the frequency for the audio is encoded. 44100 is CD quality, but you can usually get away with much lower (or use a lower amount to get a more 8-bit sound).
+	* 
+	* @method setFrequency
+	* @param {int} frequencyRate:int of the frequency you wish to encode the AudioClip at
+	* @return {LeanAudioOptions} LeanAudioOptions describing optional values
+	* @example
+	* AnimationCurve volumeCurve = new AnimationCurve( new Keyframe(0f, 1f, 0f, -1f), new Keyframe(1f, 0f, -1f, 0f));<br>
+	* AnimationCurve frequencyCurve = new AnimationCurve( new Keyframe(0f, 0.003f, 0f, 0f), new Keyframe(1f, 0.003f, 0f, 0f));<br>
+	* AudioClip audioClip = LeanAudio.createAudio(volumeCurve, frequencyCurve, LeanAudio.options().setVibrato( new Vector3[]{ new Vector3(0.32f,0f,0f)} ).setFrequency(12100) );<br>
+	*/
+	public LeanAudioOptions setFrequency( int frequencyRate ){
+		this.frequencyRate = frequencyRate;
+		return this;
+	}
+
+	/**
+	* Set details about the shape of the curve by adding vibrato modulations through it (alters the peak values giving it a wah-wah effect). You can add as many as you want to sculpt out more detail in the sound wave.
+	* 
+	* @method setVibrato
+	* @param {Vector3[]} vibratoArray:Vector3[] The first value is the period in seconds that you wish to have the vibrato wave fluctuate at. The second value is the minimum height you wish the vibrato wave to dip down to (default is zero). The third is reserved for future effects.
+	* @return {LeanAudioOptions} LeanAudioOptions describing optional values
+	* @example
+	* AnimationCurve volumeCurve = new AnimationCurve( new Keyframe(0f, 1f, 0f, -1f), new Keyframe(1f, 0f, -1f, 0f));<br>
+	* AnimationCurve frequencyCurve = new AnimationCurve( new Keyframe(0f, 0.003f, 0f, 0f), new Keyframe(1f, 0.003f, 0f, 0f));<br>
+	* AudioClip audioClip = LeanAudio.createAudio(volumeCurve, frequencyCurve, LeanAudio.options().setVibrato( new Vector3[]{ new Vector3(0.32f,0.3f,0f)} ).setFrequency(12100) );<br>
+	*/
+	public LeanAudioOptions setVibrato( Vector3[] vibrato ){
+		this.vibrato = vibrato;
+		return this;
+	}
+
+	/*
+	public LeanAudioOptions setModulation( Vector3[] modulation ){
+		this.modulation = modulation;
+		return this;
+	}*/
+
+	public LeanAudioOptions setWaveSine(){
+		this.waveStyle = LeanAudioWaveStyle.Sine;
+		return this;
+	}
+
+	public LeanAudioOptions setWaveSquare(){
+		this.waveStyle = LeanAudioWaveStyle.Square;
+		return this;
+	}
+
+	public LeanAudioOptions setWaveSawtooth(){
+		this.waveStyle = LeanAudioWaveStyle.Sawtooth;
+		return this;
+	}
+
+	public LeanAudioOptions setWaveNoise(){
+		this.waveStyle = LeanAudioWaveStyle.Noise;
+		return this;
+	}
+
+	public LeanAudioOptions setWaveStyle( LeanAudioWaveStyle style ){
+		this.waveStyle = style;
+		return this;
+	}
+
+
+	public LeanAudioOptions setWaveNoiseScale( float waveScale ){
+		this.waveNoiseScale = waveScale;
+		return this;
+	}
+
+	public LeanAudioOptions setWaveNoiseInfluence( float influence ){
+		this.waveNoiseInfluence = influence;
+		return this;
+	}
+}
+
+
