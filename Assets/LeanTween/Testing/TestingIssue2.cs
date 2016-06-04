7b972beffcc45a402cff5dcf3ec39858744b291c
Russell Savage <dentedpixel@gmail.com>
2016-06-04 13:20:02 -0400

    Trying to test a duplicate id scenario

---
+++ Assets/LeanTween/Testing/TestingIssue2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingIssue2 : MonoBehaviour {
+	public RectTransform rect;
+	public GameObject go;
+
+	// Use this for initialization
+	void Start () {
+		
+		int id1 = LeanTween.move(go,new Vector3(-.209f,-3.891f,-01.162f),2.0f).setEase(LeanTweenType.easeInQuart).id;
+		int id2 = LeanTween.rotate(rect,360f,1f).id;
+
+		Debug.Log("id1:"+id1+" 2:"+id2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
