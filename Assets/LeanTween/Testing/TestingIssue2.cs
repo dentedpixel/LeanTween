75e65000dde70f11b9c2bd51e2ca84f11522bb18
Russell Savage <dentedpixel@gmail.com>
2016-10-01 19:00:31 -0400

    Test example of forcing the passed value to something larger than 0f

--- Assets/LeanTween/Testing/TestingIssue2.cs
+++ Assets/LeanTween/Testing/TestingIssue2.cs
@@ -5,27 +5,20 @@ public class TestingIssue2 : MonoBehaviour {
 	public RectTransform rect;
 	public GameObject go;
 
-	// Use this for initialization
+	private LTDescr descr;
+
 	void Start () {
-		
-		int id1 = LeanTween.move(go,new Vector3(-.209f,-3.891f,-01.162f),2.0f).setEase(LeanTweenType.easeInQuart).id;
-		int id2 = LeanTween.rotate(rect,360f,1f).id;
-
-		Debug.Log("id1:"+id1+" 2:"+id2);
-
-		LeanTween.value(gameObject, Color.red, Color.green, 1f)
-                .setOnUpdate(OnTweenUpdate)
-                .setOnUpdateParam(new object[] { "" + 2 });
+		descr = LeanTween.move(go, new Vector3(0f,0,100f), 10f);
+		descr.passed = 5f; // this should put it at the midway
+		descr.pause(); // doesn't matter if pause after or before setting descr.passed I think if I set the passed property and paused the next frame it would work
 	}
-
-	private void OnTweenUpdate( Color update, object obj){
-		object[] objArr = obj as object[];
-		Debug.Log("update:"+update+" obj:"+objArr[0]);
-		
-	}
-	
-	// Update is called once per frame
+	bool set = false;
 	void Update () {
-	
+		if (Time.unscaledTime > 5f && !set)
+		{
+			set = true;
+			descr.resume(); // once this execute the object is put at the midway position as setted by passed and the tween continue.
+			Debug.Log("resuming");
+		}
 	}
 }
