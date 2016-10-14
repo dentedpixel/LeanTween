82dbc74cef65046cddc7c709ecf2af0c4f017ded
Russell Savage <dentedpixel@gmail.com>
2016-10-14 08:17:47 -0400

    Attempting to recreate scale punch issue

--- Assets/LeanTween/Testing/TestingIssue2.cs
+++ Assets/LeanTween/Testing/TestingIssue2.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TestingIssue2 : MonoBehaviour {
 	public RectTransform rect;
 	public GameObject go;
+	public GameObject go2;
 
 	private LTDescr descr;
 
@@ -12,6 +13,12 @@ public class TestingIssue2 : MonoBehaviour {
 		descr.passed = 5f; // this should put it at the midway
 		descr.updateNow();
 		descr.pause(); // doesn't matter if pause after or before setting descr.passed I think if I set the passed property and paused the next frame it would work
+
+//		LeanTween.scale(go2, Vector3.one * 4f, 10f).setEasePunch();
+
+		LeanTween.scaleX (go2, (go2.transform.localScale * 1.5f).x, 15f).setEase (LeanTweenType.punch);
+		LeanTween.scaleY (go2, (go2.transform.localScale * 1.5f).y, 15f).setEase (LeanTweenType.punch);
+		LeanTween.scaleZ (go2, (go2.transform.localScale * 1.5f).z, 15f).setEase (LeanTweenType.punch);
 	}
 	bool set = false;
 	void Update () {
