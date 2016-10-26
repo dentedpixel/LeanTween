abac535d952420455be80b2cb7a57832b521e6ec
Russell Savage <russell@tinybop.com>
2016-10-26 09:40:15 -0400

    Added an testing scene for seeing what might be broken in 2.40

---
+++ Assets/LeanTween/Testing/Testing240.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testing240 : MonoBehaviour {
+
+    public GameObject cube1;
+
+	// Use this for initialization
+	void Start () {
+        LTDescr lt1 = LeanTween.moveY(cube1, cube1.transform.position.y - 15.0f, 10f).setEase(LeanTweenType.easeInQuad).setDestroyOnComplete(false).setOnComplete(()=>{
+            Debug.Log("Done");
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
