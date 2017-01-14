fc032e8b2f4b3c4b0f074a203e626a3bfd0dbbee
Russell Savage <dentedpixel@gmail.com>
2017-01-14 14:16:39 -0500

    Turned tween back on in test scene.

--- Assets/LeanTween/Examples/Scripts/TestingAllCS.cs
+++ Assets/LeanTween/Examples/Scripts/TestingAllCS.cs
@@ -169,7 +169,7 @@ public class TestingAllCS : MonoBehaviour {
 		Debug.Log("updateValueExample");
 		Hashtable pass = new Hashtable();
 		pass.Add("message", "hi");
-		// LeanTween.value( gameObject, updateValueExampleCallback, ltLogo.transform.eulerAngles.y, 270f, 1f ).setEase(LeanTweenType.easeOutElastic).setOnUpdateParam(pass).setUseEstimatedTime(useEstimatedTime);
+		LeanTween.value( gameObject, updateValueExampleCallback, ltLogo.transform.eulerAngles.y, 270f, 1f ).setEase(LeanTweenType.easeOutElastic).setOnUpdateParam(pass).setUseEstimatedTime(useEstimatedTime);
 	}
 	
 	public void updateValueExampleCallback( float val, object hash ){
